c9799dd117296d2266b4bfb732df749551963a20
A H M E D <70113157+ahmed-03@users.noreply.github.com>
2024-11-24 22:05:49 +0300

    Update Fixed_Point_Addition

--- project_06.srcs/sources_1/new/Fixed_Point_Addition.sv
+++ project_06.srcs/sources_1/new/Fixed_Point_Addition.sv
@@ -6,9 +6,6 @@ module Fixed_Point_Addition #(parameter n1 = 8, n2 = 5, m1=2, m2=2 )(
     output [(n1 > n2 ? n1 : n2) + (m1 > m2 ? m1 : m2) - 1 : 0] sum
     );
     
-    localparam integer maxN = (n1 > n2) ? n1 : n2;
-    localparam integer maxM = (m1 > m2) ? m1 : m2;
-    
     assign sum = ( (m1 > m2) ? a+(b<<m1-m2) : b+(a<<m2-m1) );
 
 endmodule
